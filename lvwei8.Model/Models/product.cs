3112fbb8f0d161d260c6aa730ed7c284ae3d40f0
潜行乱世 <lizhifan520@live.com>
2018-05-19 17:37:57 +0800

    创建商品model

---
+++ lvwei8.Model/Models/product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lvwei8.Model.Models
+{
+    /// <summary>
+    /// 商品模型
+    /// </summary>
+    public partial class ProductDbModel
+    {
+        public ProductDbModel()
+        {
+
+        }
+
+
+
+    }
+    }
