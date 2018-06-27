90cfc72c8ce5fe6dde2523a73e93d50467596991
潜行乱世 <lizhifan520@live.com>
2018-06-27 10:22:27 +0800

    券卡模型

---
+++ lvwei8.Model/Models/coupon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lvwei8.Model.Models
+{
+    /// <summary>
+    /// 券卡
+    /// </summary>
+    public partial class CouponDbModel
+    {
+        /// <summary>
+        /// 券卡Id
+        /// </summary>
+        public  int CouponId { get; set; }
+    }
+}
