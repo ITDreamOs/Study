5b16c8ee2dd4b09c482b81405dbb249c32ab2941
潜行乱世 <lizhifan520@live.com>
2018-07-04 09:46:33 +0800

    券卡商户

---
+++ lvwei8.Model/Models/coupon_store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lvwei8.Model.Models
+{
+    public partial class CouponStoreDbModel
+    {
+
+        public CouponStoreDbModel()
+        {
+
+        }
+    }
+}
